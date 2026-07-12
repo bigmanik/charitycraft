ce269948056f8520317a7eeb24fee96fed8eb1d9
Bigmanik <igbinomwanhia10@gmail.com>
2026-07-12 01:18:12 +0100

    resolved the vite error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-import {defaultConfig, defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-    plugins:[tailwindcss()],
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        donate: resolve(__dirname, 'donate.html'),
+        // add every other .html page in your project root here
+      }
+    }
+  }
 })
